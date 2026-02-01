2c7a09568262f8595d2f166c2cd385f7c9bd40ab
jayziee24 <bandugularockjason@gmail.com>
2026-02-01 15:27:10 +0530

    feat: implement stroke-based undo logic for atomic deletion

--- server/index.js
+++ server/index.js
@@ -1,38 +1,66 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-    },
-})
-
-io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.id}`);
-
-    socket.on('draw_line',(data) => {
-        socket.broadcast.emit('draw_line',data);
-    });
-    socket.on('cursor_move',({x,y}) => {
-        socket.broadcast.emit('cursor_update', {
-            id : socket.id,
-            x,
-            y
-        });
-    });
-    socket.on('disconnect', (socket) => {
-        console.log(`User disconnected: ${socket.id}`);
-    });
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-const PORT = 3001
+let drawHistory = [];
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  if (drawHistory.length > 0) {
+    socket.emit("board_state", drawHistory);
+  }
+
+  socket.on("draw_line", (data) => {
+    drawHistory.push(data);
+    socket.broadcast.emit("draw_line", data);
+  });
+
+  socket.on("undo", () => {
+    if (drawHistory.length > 0) {
+      const lastItem = drawHistory[drawHistory.length - 1];
+      const lastStrokeId = lastItem.strokeId;
+
+      if (lastStrokeId) {
+        drawHistory = drawHistory.filter(
+          (item) => item.strokeId !== lastStrokeId,
+        );
+      } else {
+        drawHistory.pop();
+      }
+      io.emit("board_state", drawHistory);
+    }
+  });
+  socket.on("cursor_move", ({ x, y }) => {
+    socket.broadcast.emit("cursor_update", {
+      id: socket.id,
+      x,
+      y,
+    });
+  });
+
+  socket.on("clear", () => {
+    drawHistory = [];
+    io.emit("board_state", drawHistory);
+  });
+  socket.on("disconnect", (socket) => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = 3001;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
